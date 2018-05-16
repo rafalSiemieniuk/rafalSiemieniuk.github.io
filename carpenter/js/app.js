1e0dd05f9cb5ccd3b6baf6c86603cc55b21f2c6d
N3ro20 <n3ropl@gmail.com>
2018-05-16 23:22:42 +0200

    add: new task - carpenter

---
+++ carpenter/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var test = 'napisow';
+var jeden = '123';
+
+console.log('zbior ' + test + ' teraz');
+console.log('zbior ' + test + ' teraz');
+
+console.log('jeden ' + jeden + ' jeden');
+console.log('jeden ' + jeden + ' jeden');
